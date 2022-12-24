d43329423b7f6a50afc807199c929f9a71591a12
Muhammad-Gibran <101136529+Muhammad-Gibran@users.noreply.github.com>
2022-12-24 10:14:59 +0700

    Add files via upload

---
+++ tugas1/no3.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  var isList = [3, 10, 2002, "Samarinda"];
+  print(isList);
+
+  var isMap = Map<String, dynamic>();
+  isMap['Nama'] = 'Muhammad Gibran';
+  isMap['NPM'] = '2010631170097';
+  isMap['Target IPK'] = 3.6;
+  print(isMap);
+
+  var isSet = {'Data ke-1', 'Data ke-2', 'Data ke-3', 'Data ke-3', 'Data ke-3'};
+  print(isSet);
+}
